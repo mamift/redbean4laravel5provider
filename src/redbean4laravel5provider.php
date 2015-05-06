e47f6a5e72f72de9bac3ec3ca161f8f060be5f0d
Muhammad Miftah <mamift@gmail.com>
2015-05-06 20:25:05 +1000

    YAY.

--- src/redbean4laravel5provider.php
+++ src/redbean4laravel5provider.php
@@ -1,7 +1,7 @@
 <?php namespace Mamift\Redbean4Laravel5;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades as Laravel;
+use Illuminate\Support\Facades\Config;
 
 class Redbean4Laravel5Provider extends ServiceProvider {
 
@@ -23,20 +23,30 @@ class Redbean4Laravel5Provider extends ServiceProvider {
     public function register()
     {
         // get DB configs from app/config/database.php
-        $default = Laravel\Config::get('database.default');
-        $connections = Laravel\Config::get('database.connections');
-        
-        $db_host = $connections[$default]['host'];
-        $db_user = $connections[$default]['username']; 
-        $db_pass = $connections[$default]['password'];
-        $db_name = $connections[$default]['database'];
-        $db_driver = $connections[$default]['driver'];
+        // $default = Config::get('app_env');
+        $default = env('DB_CONNECTION');
+        // $connections = Config::get('database.connections');
+        // $connections = env()
+
+        var_dump($default);
+        // var_dump($connections);
+
+        // $db_host = $connections[$default]['host'];
+        $db_host = env('DB_HOST');
+        // $db_user = $connections[$default]['username']; 
+        $db_user = env('DB_USERNAME');
+        // $db_pass = $connections[$default]['password'];
+        $db_pass = env('DB_PASSWORD');
+        // $db_name = $connections[$default]['database'];
+        $db_name = env('DB_DATABASE');
+        // $db_driver = $connections[$default]['driver'];
+        // $db_driver = $connections[$default]['driver'];
         
         // run the R::setup command based on db_type
         if ($default != 'sqlite') {
-            $conn_string = $db_driver.':host='.$db_host.';dbname='.$db_name;
+            $conn_string = $default.':host='.$db_host.';dbname='.$db_name;
         } else {
-            $conn_string = $db_driver.':'.$db_name;
+            $conn_string = $default.':'.$db_name;
         }
 
         \R::setup($conn_string, $db_user, $db_pass);
